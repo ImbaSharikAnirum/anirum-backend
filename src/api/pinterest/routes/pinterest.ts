55b8d1101f4cbc8cd71aa665ca5df56f6aecab27
igor Sharin <sharinigor1@gmail.com>
2025-09-18 13:00:45 +0900

    fix: pinterest route

--- src/api/pinterest/routes/pinterest.ts
+++ src/api/pinterest/routes/pinterest.ts
@@ -18,21 +18,37 @@ export default {
       method: "GET",
       path: "/pinterest/status",
       handler: "pinterest.getConnectionStatus",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
     },
     {
       method: "POST",
       path: "/pinterest/disconnect",
       handler: "pinterest.disconnect",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
     },
     {
       method: "GET",
       path: "/pinterest/pins",
       handler: "pinterest.getPins",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
     },
     {
       method: "POST",
       path: "/pinterest/save-pin-as-guide",
       handler: "pinterest.savePinAsGuide",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
     },
   ],
 };
